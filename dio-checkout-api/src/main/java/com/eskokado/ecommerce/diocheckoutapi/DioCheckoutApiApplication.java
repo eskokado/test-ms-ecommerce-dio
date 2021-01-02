de43dc8e62ad1bc2a27001113996c8a818096f0a
Edson Shideki Kokado <eskokado@gmail.com>
2021-01-02 18:18:25 -0300

    Initial commit dio-checkout-api

---
+++ dio-checkout-api/src/main/java/com/eskokado/ecommerce/diocheckoutapi/DioCheckoutApiApplication.java
@@ -0,0 +1,13 @@
+package com.eskokado.ecommerce.diocheckoutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioCheckoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioCheckoutApiApplication.class, args);
+	}
+
+}
---
+++ dio-checkout-api/src/test/java/com/eskokado/ecommerce/diocheckoutapi/DioCheckoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskokado.ecommerce.diocheckoutapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioCheckoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
