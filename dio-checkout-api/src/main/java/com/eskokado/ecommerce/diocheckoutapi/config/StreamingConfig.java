376ac2df094e828370c05e41bd6ccceef9383232
Edson Shideki Kokado <eskokado@gmail.com>
2021-01-05 17:53:57 -0300

    Developed Checkout Streams

--- dio-checkout-api/src/main/java/com/eskokado/ecommerce/diocheckoutapi/config/StreamingConfig.java
+++ dio-checkout-api/src/main/java/com/eskokado/ecommerce/diocheckoutapi/config/StreamingConfig.java
@@ -5,10 +5,10 @@ import com.eskokado.ecommerce.diocheckoutapi.streaming.PaymentPaidSink;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@EnableBinding(value = {
-//        CheckoutCreatedSource.class,
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
 //        PaymentPaidSink.class
-//})
+})
 public class StreamingConfig {
 }
--- dio-checkout-api/src/main/java/com/eskokado/ecommerce/diocheckoutapi/service/CheckoutServiceImpl.java
+++ dio-checkout-api/src/main/java/com/eskokado/ecommerce/diocheckoutapi/service/CheckoutServiceImpl.java
@@ -1,10 +1,13 @@
 package com.eskokado.ecommerce.diocheckoutapi.service;
 
 import com.eskokado.ecommerce.diocheckoutapi.entity.CheckoutEntity;
+import com.eskokado.ecommerce.diocheckoutapi.event.CheckoutCreatedEvent;
 import com.eskokado.ecommerce.diocheckoutapi.repository.CheckoutRepository;
 import com.eskokado.ecommerce.diocheckoutapi.resource.checkout.CheckoutRequest;
+import com.eskokado.ecommerce.diocheckoutapi.streaming.CheckoutCreatedSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,6 +19,7 @@ import java.util.UUID;
 public class CheckoutServiceImpl implements CheckoutService {
 
     private final CheckoutRepository checkoutRepository;
+    private final CheckoutCreatedSource checkoutCreatedSource;
 
     @Override
     public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
@@ -23,6 +27,13 @@ public class CheckoutServiceImpl implements CheckoutService {
                 .code(UUID.randomUUID().toString())
                 .build();
 
-        return Optional.of(checkoutRepository.save(checkoutEntity));
+        final CheckoutEntity entity = checkoutRepository.save(checkoutEntity);
+        final CheckoutCreatedEvent checkoutCreatedEvent = CheckoutCreatedEvent.newBuilder()
+                .setCheckoutCode(entity.getCode())
+                .setStatus((entity.getStatus() == null) ? "null" : entity.getStatus().name())
+                .build();
+        checkoutCreatedSource.output().send(MessageBuilder.withPayload(checkoutCreatedEvent).build());
+
+        return Optional.of(entity);
     }
 }
