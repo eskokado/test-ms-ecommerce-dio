b3562cd3d9096aa979cc575796e860094fe6930f
Edson Shideki Kokado <eskokado@gmail.com>
2021-01-05 16:09:48 -0300

    Activating CheckoutResponse

--- dio-checkout-api/src/main/java/com/eskokado/ecommerce/diocheckoutapi/resource/checkout/CheckoutResource.java
+++ dio-checkout-api/src/main/java/com/eskokado/ecommerce/diocheckoutapi/resource/checkout/CheckoutResource.java
@@ -1,12 +1,10 @@
 package com.eskokado.ecommerce.diocheckoutapi.resource.checkout;
 
-import com.eskokado.ecommerce.checkout.event.CheckoutCreatedEvent;
+import com.eskokado.ecommerce.diocheckoutapi.entity.CheckoutEntity;
 import com.eskokado.ecommerce.diocheckoutapi.service.CheckoutService;
-import com.eskokado.ecommerce.diocheckoutapi.streaming.CheckoutCreatedSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +18,11 @@ public class CheckoutResource {
     private final CheckoutService checkoutService;
 
     @PostMapping("/")
-    public ResponseEntity<Void> create(@ModelAttribute CheckoutRequest checkoutRequest) {
-        checkoutService.create(checkoutRequest);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<CheckoutResponse> create(@ModelAttribute CheckoutRequest checkoutRequest) {
+        final CheckoutEntity checkoutEntity = checkoutService.create(checkoutRequest).orElseThrow();
+        final CheckoutResponse checkoutResponse = CheckoutResponse.builder()
+                .code(checkoutEntity.getCode())
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(checkoutResponse);
     }
 }
